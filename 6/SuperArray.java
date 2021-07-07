afa19b1f7c15119ac2c07fd176b2460ea5f17efd
Benson Leung <67038212+leungbenson@users.noreply.github.com>
2021-07-07 18:54:32 +0000

    got it to replace arrays with 100s but still have 0's in between

--- 6/SuperArray.java
+++ 6/SuperArray.java
@@ -1,4 +1,11 @@
 /**
+  Is a constructor created by default?
+  How many array elements are necesary?
+  Can we create two constructors with identical parameters?
+  If we know that an instance variable is going to have an initial value of 0, is it still good practice to include it in the constructor and set it to 0?
+  When is the this word necessary, and when can it be left out?
+  Do we need to fill in empty array indices with 0, or can we leave them at the default value?
+
    lab skeleton
    encapsulation / SuperArray
    SuperArray is a wrapper class for an array.
@@ -24,45 +31,81 @@ public class SuperArray
   //overloaded constructor -- allows specification of initial capacity
   public SuperArray( int size )
   {
-
+    data = new int[size];
+  
+    //numberElements=0;
   }
+
+ 
 
   //default constructor -- initializes capacity to 10
   public SuperArray()
   {
+    data = new int[10];
 
+    //numberElements=0;
   }
 
 
   // ~~~~~~~~~~~~~~~ METHODS ~~~~~~~~~~~~~~~
+
+
   public void add( int value )
   {
     // test to see if we need to grow, then grow
     // SIMPLE VERSION DOES NOT AUTO-GROW CAPACITY; INSERT MORE CODE HERE LATER
-
+    this.grow();
     // add item
-
-
+    data[numberElements] = value;
+    
     // increment numberElements
+    numberElements++;
 
 
   }//end add()
 
+  // New add method that takes an index parameter
+  public void add(int index, int value)
+  {
+    // test to see if we need to grow, then grow
+    this.grow(index);
+    //this.grow();
+    // add item
+    for (int i = numberElements; i>=index; i--){
+      data[i+1]=data[i];
+    }
 
+    data[index] = value;
+    
+    // increment numberElements
+    numberElements++;
+
+  }//end  new add()
+
+  //Method to test if array is empty
   public boolean isEmpty()
   {
-
-  }
+    return numberElements==0;
+  }//end of isEmpty()
 
 
   public int get(int index)
   {
+    return 0; //default return
 
   }
 
-
+  // Method for printing Array
   public String toString()
   {
+    String returnString="";
+    
+    for (int i=0; i<this.data.length;i++)
+    {
+      returnString+=(this.data[i]+" ");
+    };//end of for loop to print
+    
+    return returnString;
 
   }//end toString()
 
@@ -83,6 +126,20 @@ public class SuperArray
 
   private void grow()
   {
+    if (numberElements==this.data.length) // if new array is needed then increase by 10
+    {
+      int[] newData = new int[this.data.length+10]; //increase array by 10
+    
+    //populate newData with data
+      for (int i=0; i<this.data.length; i++){
+        newData[i] = this.data[i];
+      }
+      this.data = newData;
+      }    
+    else 
+    {
+      //do nothing
+    }
     // create a new array with extra space
     // Q: How did you decide how much to increase capacity by?
 
@@ -90,7 +147,50 @@ public class SuperArray
 
     // point data to the new array
     // Q: How does this look when illustrated using encapsulation diagram?
-
   }//end grow()
+
+// New grow method to check if index is greater than current array size or if adding a value to a full array
+
+private void grow(int index)
+  {
+    if (index>this.data.length) // if new array is needed then increase by 10
+    {
+      int[] newData = new int[index+1]; //increase array to index
+    
+    //populate newData with data
+      for (int i=0; i<this.data.length; i++){
+        newData[i] = this.data[i];
+      }
+      this.data = newData;
+    } 
+
+    else if (numberElements==this.data.length)
+      {
+      int[] newData = new int[this.data.length + 1]; //increase array by 1
+    
+      //populate newData with data
+      for (int i=0; i<this.data.length; i++){
+        newData[i] = this.data[i];
+      }
+      this.data = newData;
+      }
+
+    else{
+      //do nothing
+    }
+     
+      
+  }// end of new grow method
+
+
+
+    // create a new array with extra space
+    // Q: How did you decide how much to increase capacity by?
+
+    // copy over all the elements from the old array to the new one
+
+    // point data to the new array
+    // Q: How does this look when illustrated using encapsulation diagram?
+ 
 
 }//end class
--- 6/readme.md
+++ 6/readme.md
@@ -6,3 +6,6 @@ Demos/discussion of Time lab.
 Lecture: Encapsulation
 
 Lab work time in small groups. (implement more advanced class functionality)
+
+Notes;
+https://jamboard.google.com/d/1W-Kr7QCZDpGxqj_le0N14quS7OX7nfbZ70W3IwInke0/edit?usp=sharing
