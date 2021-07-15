25b6451f1988afc4f1f37fb73af79587dcc7c210
Benson Leung <67038212+leungbenson@users.noreply.github.com>
2021-07-15 15:27:50 +0000

    got up to set in Llist

--- DS/lists/Driver.java
+++ DS/lists/Driver.java
@@ -2,48 +2,94 @@ import java.io.*;
 import java.util.*;
 
 public class Driver{
-    public static void main(String[] args) {
-/*
-  Node n;
-	n = new Node();
-	n.setData("Eduardo");
+  public static void main(String[] args) {
+    /*
+    Node n;
+  	n = new Node();
+  	n.setData("Eduardo");
 
 
-	Node n2 = new Node("Brian");
-	n.setNext(n2);
+  	Node n2 = new Node("Brian");
+  	n.setNext(n2);
 
-	System.out.println(n);
+  	System.out.println(n);
 
-	System.out.println(n2);
+  	System.out.println(n2);
 
-	System.out.println(n.getNext());
+  	System.out.println(n.getNext());
+
+  //Node n3 = new Node("Steph");
+
 	n.getNext().setNext(new Node("Steph"));
-	System.out.println(n2.getNext());
-	System.out.println(n.getNext().getNext());
-*/
-	Node L;
+  n.getNext().getNext().setNext(new Node("Tofr"));
+	//System.out.println(n2.getNext());
+	//System.out.println(n.getNext().getNext());
+
+  //create new list
+	Node L = new Node("Tiffany");
+  L.setNext(n);
+
+  System.out.println("Create a new  list that looks like: L->a->b->c->d ");
+  L.printNodeList();
+
+  //System.out.println(L);
+
+  //System.out.println(n.getNext().getNext().getNext());
+
+  //insert new node in linked list
+  Node x = new Node("Mamudu");
+  x.setNext(n2); //Mamudu to Brian
+  n.setNext(x); //Eduardo to Mamudu
+
+  System.out.println("Write the code to insert an x between b and c");
+  L.printNodeList();
+
+  //code to delete a node
+  x.setNext(n2.getNext());
+  System.out.println("Write the code to delete c");
+  L.printNodeList();
+  //Node
 	// 1.Create a new  list that looks like:
 	//   L->"a"->"b"->"c"->"d"
-  L = new Node("a");
-
-  Node L2 = new Node("b");
-  L.setNext(L2);
-  Node L3 = new Node("c");
-  L2.setNext(L3);
-  Node L4 = new Node("d");
-  L3.setNext(L4);
-
-
-
-  // 2. Write the code to insert an "x"
+	// 2. Write the code to insert an "x"
 	//    between the b and the c
-//  Node LX = new Node("x");
-  L2.setNext(new Node("x"));
-  L2.getNext().setNext(L2);
-  String output = L.printList();
-  System.out.println(output);
 	// 3. Write the code to delete the c
 
+*/
 
-  }// main
-}//end class
+  //Call Llist.java file
+  System.out.println("\n");
+	System.out.println("Llist");
+	System.out.println("-----------");
+
+	Llist ll = new Llist();
+  System.out.println(ll);
+  System.out.println("length = " + ll.length());
+  System.out.println(ll.isEmpty());
+
+  //Call Llist.java file
+  System.out.println("\n");
+  System.out.println("Llist");
+  System.out.println("-----------");
+  ll.addFront("A");
+	System.out.println(ll);
+  System.out.println("length = " + ll.length());
+
+  //Call Llist.java file
+  System.out.println("\n");
+	System.out.println("Llist (updated)");
+	System.out.println("-----------");
+
+  ll.addFront("B");
+  ll.addFront("C");
+  ll.addFront("D");
+  System.out.println(ll);
+  ll.set(2, "Tiffany");
+  System.out.println(ll);
+  System.out.println("length = " + ll.length());
+
+  //check get() method
+  //System.out.println(ll.get(0));
+  //System.out.println(ll.get(1));
+    }
+}
